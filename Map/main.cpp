9e85ec37c4f7baa95a5ed65bc2b415a0d334749e
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-04-03 00:49:16 +0300

    Added code from 1st attempt to see if I could create a 'smart' map function.
    The VS2010 solution is new.
    The code was extracted from a source file created in Autumn 2008.
    
    The code is included here for completeness.
    
    A 2nd attempt to create a smart map function lead to the working version of map
    included in the Array project.
    
    --HG--
    branch : 1st Experiment

--- Map/main.cpp
+++ Map/main.cpp
@@ -7,8 +7,37 @@ using std::endl;
 using std::system;
 
 
+template<typename T>
+struct Array
+{
+	T x;
+};
+
+template<typename T>
+struct Func
+{
+};
+
+template<typename T>
+Array<T> map(Array<T> &a, Func<T> &f)
+{
+	cout<<"base"<<endl;
+	return a;
+}
+
+template<typename T,typename E>
+Array<T> map(Array<T> &a, Func<E> &f)
+{
+	cout<<"step"<<endl;
+	map(a.x,f);
+	return a;
+}
+
 int main()
 {
+	Array<Array<Array<int> > > arr;
+	Func<int> func;
+	map(arr,func);
 
 	system("pause");
 	return 0;
