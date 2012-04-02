942e88cea311cb470f91b04d4f6d401acafd2e4f
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-04-03 00:46:31 +0300

    Created a VS2010 solution for the 'Map' project.
    This repository will keep track of the attempts to create a 'smart' map function
    and the final working version.

---
+++ Map/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::endl;
+
+#include <cstdlib>
+using std::system;
+
+
+int main()
+{
+
+	system("pause");
+	return 0;
+} // end function main
