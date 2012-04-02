afad5b66812a0764036eddd8da60def1ed012b46
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-04-03 02:19:09 +0300

    Created 2 overloaded versions of a function such that the compiler would select the one returning false. And if the one returning false was to be commented the other would be selected instead.
    This is trivial, yes.
    
    Also experimented creating a helper class that can be derived from exactly 2 types.
    
    --HG--
    branch : Testing

--- Map/main.cpp
+++ Map/main.cpp
@@ -1,15 +1,48 @@
 #include <iostream>
-using std::cout;
-using std::cin;
+using std::wcout;
+using std::wcin;
 using std::endl;
 
 #include <cstdlib>
 using std::system;
 
+#include <type_traits>
+using std::true_type;
+using std::false_type;
+using std::integral_constant;
+using std::is_same;
+using std::is_convertible;
+
+class Dummy{};
+
+
+bool fun(const Dummy &dummy)
+{
+	return false;
+} // end function fun
+
+
+bool fun(Dummy &dummy)
+{
+	return true;
+} // end function fun
+
+
+template<typename T>
+struct Test : T
+{
+	static_assert(is_same<T,true_type>::value || is_same<T,false_type>::value,"T is neither true_type nor false_type");
+};
+
 
 int main()
 {
+	wcout << std::boolalpha;
+	Test<true_type> t;
 
+	wcout << t.value << endl;
+
+	wcout << fun(Dummy()) << endl;
 	system("pause");
 	return 0;
 } // end function main
