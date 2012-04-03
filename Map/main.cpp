617d8ff2331816ef18474829aaca89454e926455
Vaptistis Anogeianakis <gd.nightmare@gmail.com>
2012-04-03 03:00:22 +0300

    Started testing map. Modified code of map to solve initial problems.
    Used class template explicit specializations to avoid having both block of code
    used by map be compiled together (since usually only one of them is semantically
    correct at a time).

--- Map/main.cpp
+++ Map/main.cpp
@@ -3,6 +3,10 @@ using std::wcout;
 using std::wcin;
 using std::endl;
 
+#include <iomanip>
+using std::setw;
+using std::setprecision;
+
 #include <cstdlib>
 using std::system;
 
@@ -42,20 +46,30 @@ template<typename IIter, typename OIter, typename Func>
 auto checkRecursiveStep(IIter inBegin, IIter inEnd, OIter outBegin, Func f, const Dummy &dummy)->false_type;
 
 
+
 template<typename IIter, typename OIter, typename Func>
-void map(IIter inBegin, IIter inEnd, OIter outBegin, Func f)
+void map(IIter inBegin, IIter inEnd, OIter outBegin, Func f);
+
+template<bool active> struct Map;
+
+template<>
+struct Map<true>
 {
-	if(Workaround<decltype(checkBaseCase(inBegin,inEnd,outBegin,f,dummy))>::value)	// if this block of code would be semantically correct...
-	{																				// ...run this block
+	template<typename IIter, typename OIter, typename Func>
+	static void mapBaseCase(IIter inBegin, IIter inEnd, OIter outBegin, Func f)
+	{
 		while(inBegin != inEnd)
 		{
 			*outBegin = f(*inBegin);
 			++inBegin;
 			++outBegin;
 		} // end while
-	}
-	else if(Workaround<decltype(checkRecursiveStep(inBegin,inEnd,outBegin,f,dummy))>::value)	// if that block of code would be semantically correct...
-	{																							// ...run that block
+	} // end function mapBaseCase
+
+
+	template<typename IIter, typename OIter, typename Func>
+	static void mapRecursiveStep(IIter inBegin, IIter inEnd, OIter outBegin, Func f)
+	{
 		//using std::begin;
 		//using std::end;
 
@@ -65,10 +79,37 @@ void map(IIter inBegin, IIter inEnd, OIter outBegin, Func f)
 			++inBegin;
 			++outBegin;
 		} // end while
-	} // end if
+	} // end function mapRecursiveStep
+}; // end struct Map
 
-	static_assert(!Workaround<decltype(checkBaseCase(inBegin,inEnd,outBegin,f,dummy))>::value
-		&& !Workaround<decltype(checkRecursiveStep(inBegin,inEnd,outBegin,f,dummy))>::value,
+template<>
+struct Map<false>
+{
+	template<typename IIter, typename OIter, typename Func>
+	static void mapBaseCase(IIter inBegin, IIter inEnd, OIter outBegin, Func f)
+	{
+		// do nothing
+	} // end function mapBaseCase
+
+	template<typename IIter, typename OIter, typename Func>
+	static void mapRecursiveStep(IIter inBegin, IIter inEnd, OIter outBegin, Func f)
+	{
+		// do nothing
+	} // end function mapRecursiveStep
+}; // end struct Map
+
+
+
+template<typename IIter, typename OIter, typename Func>
+void map(IIter inBegin, IIter inEnd, OIter outBegin, Func f)
+{
+	Map<Workaround<decltype(checkBaseCase(inBegin,inEnd,outBegin,f,dummy))>::value>
+		::mapBaseCase(inBegin,inEnd,outBegin,f);
+	Map<!Workaround<decltype(checkBaseCase(inBegin,inEnd,outBegin,f,dummy))>::value
+		&& Workaround<decltype(checkRecursiveStep(inBegin,inEnd,outBegin,f,dummy))>::value>
+			::mapRecursiveStep(inBegin,inEnd,outBegin,f);
+	static_assert(Workaround<decltype(checkBaseCase(inBegin,inEnd,outBegin,f,dummy))>::value
+		|| Workaround<decltype(checkRecursiveStep(inBegin,inEnd,outBegin,f,dummy))>::value,
 			"Neither the base case nor the recursive step of map can be applied to this combination of iterators and function");
 } // end function map
 
@@ -88,20 +129,71 @@ int h(char *s)
 	return strlen(s);
 }
 
+
+const unsigned size = 7;
+const unsigned rows = 3;
+vector<double> vd(size);
+vector<int> vi(size);
+//vector<double> vd2(size);
+vector<vector<int>> vvi(rows,vector<int>(size));
+vector<vector<double>> vvd(rows,vector<double>(size));
+
+void printAll()
+{
+	wcout << "----------------------------\n";
+	for(auto i = 0u ; i < vd.size() ; i++)
+		wcout << setw(6) << vd[i];
+	wcout << "\n\n";
+
+	for(auto i = 0u ; i < vi.size() ; i++)
+		wcout << setw(6) << vi[i];
+	wcout << "\n\n";
+
+	for(auto i = 0u ; i < vvi.size() ; i++)
+	{
+		for(auto j = 0u ; j < vvi[i].size() ; j++)
+			wcout << setw(6) << vvi[i][j];
+		wcout << "\n";
+	} // end for
+	wcout << "\n\n";
+
+	for(auto i = 0u ; i < vvd.size() ; i++)
+	{
+		for(auto j = 0u ; j < vvd[i].size() ; j++)
+			wcout << setw(6) << vvd[i][j];
+		wcout << "\n";
+	} // end for
+	wcout << "\n\n";
+} // end printAll
+
+
 int main()
 {
-	wcout << std::boolalpha;
+	wcout << std::boolalpha << setprecision(1) << std::fixed;
 
-	const unsigned size = 7;
-	vector<double> vd(size);
-	vector<int> vi(size);
-	vector<double> vd2(size);
-	vector<vector<int>> vvi(size,vector<int>(size));
-	vector<vector<double>> vvd(size,vector<double>(size));
+
+	// initializations
+	for(auto i = 0u ; i < vd.size() ; i++)
+		vd[i] = i*1.1;
+
+	for(auto i = 0u ; i < vi.size() ; i++)
+		vi[i] = i;
+	
+	for(auto i = 0u ; i < vvi.size() ; i++)
+		for(auto j = 0u ; j < vvi[i].size() ; j++)
+			vvi[i][j] = i*vvi.size()+j;
+
+	for(auto i = 0u ; i < vvd.size() ; i++)
+		for(auto j = 0u ; j < vvd[i].size() ; j++)
+			vvd[i][j] = (i*vvd.size()+j)*1.1;
+
+	printAll();
 
 	wcout << "vi->vd\n";
+	map(vi.begin(),vi.end(),vd.begin(),f);
 	wcout << "checkBaseCase: " << Workaround<decltype(checkBaseCase(vi.begin(),vi.end(),vd.begin(),f,dummy))>::value << endl;
 	wcout << "checkRecursiveStep: " << Workaround<decltype(checkRecursiveStep(vi.begin(),vi.end(),vd.begin(),f,dummy))>::value << endl;
+	printAll();
 	wcout << "\n";
 	wcout << "vvi->vvd\n";
 	wcout << "checkBaseCase: " << Workaround<decltype(checkBaseCase(vvi.begin(),vvi.end(),vvd.begin(),f,dummy))>::value << endl;
